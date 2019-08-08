f3a25399e6dc8ddaf26c9c3fcba89599c9352826
Anonymous <caijunruchina@163.com>
2019-08-08 20:26:21 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,12 @@
 function srh() {
     var string = document.getElementById("ipt").value;
     /*var url = "https://www.coolapk.com/search?q=" + string;*/
-    var url = "https://www.baidu.com/#ie=utf-8&wd=" + string;
-    window.location.href=url;
+    if( string == "" || string == null || string == undefined){
+        alert("搜索内容不能为空!")
+    } else {
+        var url = "https://www.baidu.com/#ie=utf-8&wd=" + string;
+        window.location.href=url;
+    }
 }
 
 function key() {
